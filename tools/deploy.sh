9c225577934af1c889238fc2c78ecac2ba281441
hugo <hugohuang1111@gmail.com>
2021-03-14 00:36:44 +0800

    git action

---
+++ tools/deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+# vuepress dist folder
+cd $GITHUB_WORKSPACE/docs/.vuepress/dist
+
+git init
+git config user.name "HugoHuang" --local
+git config user.email "hugohuang1111@gmail.com" --local
+
+# Generate a CNAME file
+# echo $CNAME > CNAME
+
+DEPLOY_REPO="https://x-access-token:${ACCESS_TOKEN}@github.com/innocenttimes/UnrealPluginDocs.git"
+
+git branch -m gh-pages
+git add .
+git commit -m "Deploy"
+git push --force $DEPLOY_REPO gh-pages
+rm -fr .git
+
+cd $GITHUB_WORKSPACE
+
+echo "Successfully deployed!"
